6f85f2f8937e511199b2aac74ea1b18d2e9df77c
ArtiaEle <xydeng@163.com>
2025-10-31 16:20:18 +0800

    撤销掉那些打印的操作

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,6 @@ import xhr from './xhr';
 
 function axios(config: AxiosRequestConfig):void {
    xhr(config);
-   console.log(config);
-   console.log("随便写点什么，上传到远程服务器，十一是个大帅逼！");
-   console.log("2222222十一是个大帅逼！");
 }
 
 
